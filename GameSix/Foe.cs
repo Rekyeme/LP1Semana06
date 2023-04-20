dc8713c1c54214412f2b3904d5a8b8d9f292603b
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-04-20 15:39:32 +0100

    Add variables & constructor

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -2,6 +2,17 @@ namespace GameSix
 {
     public class Foe
     {
-        
+        //Class instance variables.
+        private string name;
+        private float health;
+        private float shield;
+
+        //Class constructor
+        public Foe(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
