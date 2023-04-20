6a7eb40bbc267efeb3e1421645b895f436a5ee18
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-04-20 15:52:21 +0100

    Added 2 methods

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -2,17 +2,36 @@ namespace GameSix
 {
     public class Foe
     {
-        //Class instance variables.
+        // Foe Class instance variables.
         private string name;
         private float health;
         private float shield;
 
-        //Class constructor
+        // Foe Class constructor.
         public Foe(string name)
         {
             this.name = name;
             health = 100;
             shield = 0;
+        }
+
+        // Obtain the Foe name.
+        public string GetName()
+        {
+            return name;
+        }
+
+        // Damage inflicted to the Foe.
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }    
         }
     }
 }
